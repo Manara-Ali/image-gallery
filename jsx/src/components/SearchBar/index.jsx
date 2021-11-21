5f1b8ba686cf9275f485539014d1e33f0b2ac058
Manara Ali <manaraali64@gmail.com>
2021-11-21 15:55:27 -0500

    Create search bar

---
+++ jsx/src/components/SearchBar/index.jsx
@@ -0,0 +1,18 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. ADDITIONAL IMPORTS
+
+// 3. CREATE A CLASS BASED COMPONENT
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Search Bar</h1>
+      </div>
+    );
+  }
+}
+
+// 4. EXPORT COMPONENT TO BE USED IN OTHER PARTS OF OUR APPLICATION
+export default SearchBar;
