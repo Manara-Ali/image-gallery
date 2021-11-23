9284527e9c83f1675f5661c61a1dfa7c296e1729
Manara Ali <manaraali64@gmail.com>
2021-11-23 18:35:08 -0500

    Import ImageCard into the ImageList component to render a single image

--- jsx/src/components/ImageList/index.jsx
+++ jsx/src/components/ImageList/index.jsx
@@ -2,16 +2,15 @@
 import React from "react";
 
 // 2. ADDITIONAL IMPORTS
+import ImageCard from "../ImageCard";
 
 // 3. CREATE A FUNCTION BASED COMPONENT
 const ImageList = (props) => {
   const listOfImages = props.images.map((element) => {
     return (
-      <img
-        key={element.id}
-        src={element.urls.regular}
-        alt={element.description}
-      />
+      <div key={element.id}>
+        <ImageCard element={element} />
+      </div>
     );
   });
   return <div>{listOfImages}</div>;
