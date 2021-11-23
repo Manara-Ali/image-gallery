70e1c32857c631add8b311446bd4c26448cf41dc
Manara Ali <manaraali64@gmail.com>
2021-11-23 18:04:40 -0500

    Make use of axios for network request

--- jsx/src/components/App/index.jsx
+++ jsx/src/components/App/index.jsx
@@ -2,13 +2,31 @@
 import React from "react";
 
 // 2. ADDITIONAL IMPORTS
+import unsplashAPI from "../api/unsplash";
 import SearchBar from "../SearchBar";
 
 // 3. CREATE FUNCTION BASED COMPONENT
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: [],
+    };
+  }
+
   // Create a method to retrieve user search term
-  handleOnSubmit = (term) => {
-    console.log(term);
+  handleOnSubmit = async (term) => {
+    const response = await unsplashAPI.get("/search/photos", {
+      params: {
+        query: `${term}`,
+      },
+    });
+
+    // Set the response into state
+    this.setState({
+      images: response.data.results,
+    });
   };
   render() {
     return (
